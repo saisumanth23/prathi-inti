3eb09869a8b6a3541126368347d7c7ac3369cb5b
Sai Sumanth Kasula <33429653+saisumanth23@users.noreply.github.com>
2023-02-24 22:22:49 -0600

    updated changes

---
+++ src/components/Products/product/Product.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton} from "@mui/material"
+import { AddShoppingCart } from '@mui/icons-material'
+
+const Product = ({product}) => {
+
+  return (
+    <div>
+        <Card style={{maxWidth:"100%"}}>
+            <CardMedia style={{height: 0,paddingTop: "56.25%"}} image = "" title={product.name}></CardMedia>
+            <CardContent>
+                <div style={{display: "flex",justifyContent: "space-between"}}>
+                    <Typography variant='h5' gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant='h5' gutterBottom>
+                        {product.price}
+                    </Typography>
+
+                </div>
+                <Typography variant='h2' color= "textSecondary">
+                    {product.description}
+                </Typography>
+                <CardActions disableSpacing style={{  display: "flex",justifyContent: "flex-end"}}>
+                    <IconButton aria-label='Add to Cart'>
+                        <AddShoppingCart/>
+                    </IconButton>
+                </CardActions>
+            </CardContent>
+
+        </Card>
+      
+    </div>
+  )
+}
+
+export default Product
+
